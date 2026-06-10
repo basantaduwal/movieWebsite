82e993d31b57b718d06bc59bdbf853070bb537bb
basantaduwal <basantaduwal10@gmail.com>
2026-06-10 14:17:41 +0545

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MovieCard from './components/movieCard'
+
+function App() {
+  return (
+    <>
+    <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
+    </>
+  )
+}
+
+export default App
